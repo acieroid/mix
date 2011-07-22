7391f063c8943abf34b558f0cc084fa8e4fd1a96
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-07-22 09:12:02 +0200

    handle multiple mixers with F1/F2

--- src/main.c
+++ src/main.c
@@ -18,31 +18,73 @@ void exit_ncurses()
   endwin();
 }
 
+Manager *next_manager(MixList *list, Manager *selected)
+{
+  MixList *iterator;
+  mix_foreach(iterator, list) {
+    if (list->data == selected) {
+      if (list->next == NULL)
+        return selected; /* already at right-most element */
+      else
+        return list->next->data;
+    }
+  }
+  return selected; /* we shouldn't be here */
+}
+
+Manager *prev_manager(MixList *list, Manager *selected)
+{
+  MixList *iterator = NULL;
+  Manager *prev = NULL;
+  mix_foreach(iterator, list) {
+    if (list->data == selected) {
+      if (prev == NULL)
+        return selected; /* already at left-most element */
+      else
+        return prev;
+    }
+    prev = iterator->data;
+  }
+  return selected; /* we shouldn't be here neither */
+}
+
 int main(int argc, char *argv[])
 {
   int ch = 0;
   MixAPIFD fd;
-  MixMixer *mixer;
-  Manager *manager;
+  MixList *mixers = NULL, *managers = NULL, *iterator = NULL;
+  Manager *selected = NULL;
 
   init_ncurses();
   fd = mix_open_dev("/dev/mixer");
-  mixer = mix_get_mixer(fd, 0);
-  /* TODO: create a manager for each mixer, not only the first one */
-  manager = manager_new(mixer);
-  manager_draw(manager);
+  mixers = mix_get_mixers(fd);
+  mix_foreach(iterator, mixers) {
+    managers = mix_list_prepend(managers, manager_new(iterator->data));
+    if (selected == NULL)
+      selected = managers->data; /* select the first mixer */
+  }
+  managers = mix_list_reverse(managers);
+
   refresh();
+  manager_draw(selected);
 
   while (ch != QUIT_KEY) {
+    /* handle key press */
     ch = getch();
+    if (ch == NEXT_MIXER_KEY)
+      selected = next_manager(managers, selected);
+    else if (ch == PREV_MIXER_KEY)
+      selected = prev_manager(managers, selected);
+    manager_key_pressed(selected, ch);
+
+    /* and redraw */
     clear();
-    manager_key_pressed(manager, ch);
     refresh();
-    manager_draw(manager);
+    manager_draw(selected);
   }
 
-  manager_free(manager);
-  mix_mixer_free(mixer);
+  mix_list_free(managers, (MixFreeFunc) manager_free);
+  mix_list_free(mixers, (MixFreeFunc) mix_mixer_free);
   exit_ncurses();
   return 0;
 }
